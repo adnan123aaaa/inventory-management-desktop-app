a88674b3ae610fceebf40e09da8646c2819f3c39
adnan123aaaa <adnanalies@gmail.com>
2025-07-14 22:50:54 +0530

    Add src/main/main.js 1752513652837

---
+++ src/main/main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  const startURL = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../../renderer/build/index.html')}`;
+
+  win.loadURL(startURL);
+
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+// IPC handlers can be added here for communication between main and renderer processes
