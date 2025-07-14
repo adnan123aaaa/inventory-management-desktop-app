350a0e56eccc426cfb94710baeff23348b4c2dfa
adnan123aaaa <adnanalies@gmail.com>
2025-07-14 22:50:55 +0530

    Add src/main/preload.js 1752513654806

---
+++ src/main/preload.js
@@ -0,0 +1,17 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  send: (channel, data) => {
+    // whitelist channels
+    const validChannels = ['toMain'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  receive: (channel, func) => {
+    const validChannels = ['fromMain'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+  }
+});
